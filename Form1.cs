c94bc822911b8aa8ee5d941bc53288f123259a8e
NewofTheSimp <wvinke550@gmail.com>
2024-12-30 13:00:48 +0100

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Zipper
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            byte[] file = stats.readFileAsBytes();
+            uint[] freq = stats.countEachByte(file);
+            DoublyLinkedList DLL = stats.makeDLL(freq);
+            DLL = stats.makeDLLHuffman(DLL);
+            //Node tail = stats.CreateTree(DLL.Head);
+            Dictionary<byte, string> table = stats.BuildHuffmanTable(DLL.Tail);
+
+        }
+
+    }
+}
+
